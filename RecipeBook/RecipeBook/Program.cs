57a70c2a25d5682c7f9686f1c7441f10be3f73f9
vVendigo <ezurawik@poczta.fm>
2018-11-28 22:23:13 +0100

    Created the first version of the RecipeBook program which has no features as of yet. You can only exit a program by typing "0" and confirming it with the Enter key. The program has no GUI now, it is a simple console aplication and has only one class for now: Recipe.

---
+++ RecipeBook/RecipeBook/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple RecipeBook");
+            bool done = false;
+
+            const int maxNumber = 100;
+            Recipe[] recipes = new Recipe[maxNumber];
+
+            // Recipes are made NULL
+            for(int i = 0; i < maxNumber; i++)
+            {
+                recipes[i] = null;
+            }
+
+            while(!done)
+            {
+                Console.WriteLine("Select an action: ");
+                Console.WriteLine("1. Add a recipe.");
+                Console.WriteLine("2. Open a recipe.");
+                Console.WriteLine("0. Exit.");
+
+                int choice = ReadInt("", 0, 3);
+
+                switch(choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 0:
+                        done = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Reading STRING
+        static string ReadString(string prompt = "Enter text: ", string error = "You must enter at least one character!")
+        {
+            bool isGood = false;
+            string wynik;
+            do
+            {
+                Console.Write(prompt);
+                wynik = Console.ReadLine();
+                if (wynik.Length >= 1)
+                    isGood = true;
+                if (!isGood)
+                    Console.WriteLine(error);
+            } while (!isGood);
+            return wynik;
+        }
+        // Reading INT
+        static int ReadInt(string prompt = "Enter a number: ", int min = int.MinValue, int max = int.MaxValue, string error = "You have entered a wrong number!")
+        {
+            bool isGood = false;
+            int wynik;
+            do
+            {
+                bool isDouble = int.TryParse(ReadString(prompt), out wynik);
+                if ((wynik >= min) && (wynik <= max) && isDouble)
+                    isGood = true;
+                if (!isGood)
+                    Console.WriteLine(error);
+            } while (!isGood);
+            return wynik;
+        }
+    }
+}
---
+++ RecipeBook/RecipeBook/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook
+{
+    class Recipe
+    {
+        public string Name { get; }
+        public string Ingredients { get; }
+        public string Preparations { get; }
+
+        public Recipe(string name, string ingredients, string preparations)
+        {
+            Name = name;
+            Ingredients = ingredients;
+            Preparations = preparations;
+        }
+    }
+}
